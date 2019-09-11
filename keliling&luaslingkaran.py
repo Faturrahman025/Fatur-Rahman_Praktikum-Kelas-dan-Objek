70c5b4d3fae303adda046de364c7baafa7f36afa
Your Name <you@example.com>
2019-09-11 08:37:23 +0700

    praktikum

--- keliling&luaslingkaran.py
+++ keliling&luaslingkaran.py
@@ -3,7 +3,9 @@ class lingkaran(object):
       self.phi = p
       self.jarijari = r
    def hitungkeliling(self):
-      return 2* self.phi * self.jarijari 
+      return 2* self.phi * self.jarijari
+   def hitungluas(self):
+      return self.phi * self.jarijari * self.jarijari
 
 def main():
    lingkaran1 = lingkaran(3.14, 25)
@@ -19,6 +21,21 @@ def main():
    print('phi        :', lingkaran2.phi)
    print('jari-jari  :', lingkaran2.jarijari)
    print("Keliling lingkaran =", lingkaran2.hitungkeliling())
+
+
+   lingkaran1 = lingkaran(3.14, 25)
+
+   print('\nObjek lingkaran 1')
+   print('phi       : ', lingkaran1.phi)
+   print('jari-jari : ', lingkaran1.jarijari)
+   print('Luas lingkaran  = ', lingkaran1.hitungluas())
+
+   lingkaran2 = lingkaran(3.14, 5)
+
+   print("\nObjek lingkaran 2")
+   print('phi       : ', lingkaran2.phi)
+   print('jari-jari : ', lingkaran2.jarijari)
+   print("Luas lingkaran  = ", lingkaran2.hitungluas())
 
 if __name__ == "__main__":
    main()
